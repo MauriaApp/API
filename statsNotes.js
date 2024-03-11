4117f4a5ffdbd07d36e99370b4140dd774033e24
MiloMontuori <milo.montuori@student.junia.com>
2024-03-11 15:53:40 +0100

    up

--- statsNotes.js
+++ statsNotes.js
@@ -16,25 +16,25 @@ async function PostStatsNotes(username, password, shared = false) {
 
     // console.log(username);
 
-    if (shared) {
-        try {
-            axios(API_Stats + "/update-grades", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": "Bearer " + TOKEN_JUNIANALYTICS,
-                },
-                data: JSON.stringify({
-                    "email": username,
-                    "grades": response,
-                }),
-            })
+    // if (shared) {
+    //     try {
+    //         axios(API_Stats + "/update-grades", {
+    //             method: "POST",
+    //             headers: {
+    //                 "Content-Type": "application/json",
+    //                 "Authorization": "Bearer " + TOKEN_JUNIANALYTICS,
+    //             },
+    //             data: JSON.stringify({
+    //                 "email": username,
+    //                 "grades": response,
+    //             }),
+    //         })
 
-        }
-        catch (e) {
-            console.log(e, "PROBLEME CHEZ JUNIANALYTICS");
-        }
-    }
+    //     }
+    //     catch (e) {
+    //         console.log(e, "PROBLEME CHEZ JUNIANALYTICS");
+    //     }
+    // }
 
     return response;
 }
@@ -44,22 +44,24 @@ async function PostStatsNotes(username, password, shared = false) {
 
 
 async function GetStatsNotes(username, password) {
-    let USERNAME = username.substring(0, username.indexOf("@"));
+    // let USERNAME = username.substring(0, username.indexOf("@"));
 
-    const response = await axios(API_Stats + "/grades-stats?user=" + USERNAME, {
-        headers: {
-            "Authorization": "Bearer " + TOKEN_JUNIANALYTICS,
-        },
-    });
-    if (response.data) {
-        let noteStats = response.data;
-        return noteStats;
-    }
+    // const response = await axios(API_Stats + "/grades-stats?user=" + USERNAME, {
+    //     headers: {
+    //         "Authorization": "Bearer " + TOKEN_JUNIANALYTICS,
+    //     },
+    // });
+    // if (response.data) {
+    //     let noteStats = response.data;
+    //     return noteStats;
+    // }
 
-    if (response.data.error) {
-        await PostStatsNotes(username, password, true);
-        return GetStatsNotes(username, password);
-    }
+    // if (response.data.error) {
+    //     await PostStatsNotes(username, password, true);
+    //     return GetStatsNotes(username, password);
+    // }
+
+    return(await PostStatsNotes(username, password, true));
 
 }
 
